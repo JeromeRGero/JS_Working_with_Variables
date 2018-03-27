961bbbfaf4818888c2a836c36b120547f0448938
JeromeRGero <JeromeRGero@gmail.com>
2018-03-27 08:50:55 -0700

    Variable Demo

---
+++ README.md
@@ -0,0 +1,2 @@
+This is just a simple demonstration of how variables can be declared, 
+defined, and output to the console.
---
+++ answers.js
@@ -0,0 +1,17 @@
+var num1 = 1, num2 = 5, num3 = 12, birthdayNum = 17, 
+message1 = "Hello ", school1 = "Coding ", school2 = "Dojo ",
+boolean1 = false, boolean2 = true, ua;
+
+console.log("This is my first string" + message1);
+console.log("Part one to the name of my school" + school1);
+console.log("Part two to the name of my school" + school2);
+
+console.log("Slash n writen '\\n'\nis what I can use to" + 
+"print out to the next line: \n" +  num1 + ' ' +  
+num2 + ' ' +  num3 + ' \n' + 
+"And my birthday which is May" + birthdayNum +
+". \nBut wait! \nIs that true? Lets check boolean2: " + 
+boolean2 + ". \n Wow! I guess so cause boolean1 wasr" +
+boolean1 + ". \n So it looks like everyone of theses " +
+"at least wasn't undefined like " + ua + 
+". \n...Cools stuff.");
